c89116722999151533fd56068ae4679ce8b88227
akrem2005 <lighttech96@gmail.com>
2024-02-10 10:01:49 +0000

    fix

--- src/layouts/dashboard/header.jsx
+++ src/layouts/dashboard/header.jsx
@@ -34,15 +34,7 @@ export default function Header({ onOpenNav }) {
         </IconButton>
       )}
 
-      <Searchbar />
-
       <Box sx={{ flexGrow: 1 }} />
-
-      <Stack direction="row" alignItems="center" spacing={1}>
-        <LanguagePopover />
-        <NotificationsPopover />
-        <AccountPopover />
-      </Stack>
     </>
   );
 
--- src/pages/app.jsx
+++ src/pages/app.jsx
@@ -8,7 +8,7 @@ export default function AppPage() {
   return (
     <>
       <Helmet>
-        <title> Dashboard | Minimal UI </title>
+        <title> Dashboard | AradaX </title>
       </Helmet>
 
       <AppView />
